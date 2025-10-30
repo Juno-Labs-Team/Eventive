dbc9bae6172f6826adec07df40652ae72fb4cdaf
λamdashi <orangejuiceplz12321@gmail.com>
2025-10-30 17:35:59 -0400

    fix: fixed code

--- tsa-project/src/App.tsx
+++ tsa-project/src/App.tsx
@@ -1,69 +1,45 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./components/navbar.tsx";
+import Account from "./pages/account.tsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-<<<<<<< HEAD
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <div>
-                <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-              </div>
-              <h1>Vite + React</h1>
-              <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button>
-                <p>
-                  Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-              </div>
-              <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-              </p>
-            </>
-          } />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </BrowserRouter>
-=======
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <BrowserRouter>
       <Navbar />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
->>>>>>> parent of 04325fc (fix: fix mason's noob code)
-    </>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div>
+              <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </>
+        } />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
