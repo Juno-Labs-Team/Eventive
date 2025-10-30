d51d538fb044e2358c917a7dffdaa49569f6e040
Mace <153683321+127msafran@users.noreply.github.com>
2025-10-30 21:17:40 +0000

    Start work on navbar.tsx

--- tsa-project/src/components/navbar.tsx
+++ tsa-project/src/components/navbar.tsx
@@ -1,8 +1,15 @@
-//Place holder for future navbar component
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import Account from "../pages/account.tsx";
+
 export default function Navbar() {
     return (
         <>
-            
+            <BrowserRouter>
+                <Routes>
+                    <Route path="account" element={<Account />}/>
+                    <Link to="/account"/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
--- tsa-project/src/pages/account.tsx
+++ tsa-project/src/pages/account.tsx
@@ -1,2 +1,10 @@
 //user account page
 //should show user info here
+
+export default function Account() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
